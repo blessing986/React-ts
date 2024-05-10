7bd2e329e917f468b67f3a82199d2244dcbf3dc0
Blessing U <blessing986@users.noreply.github.com>
2024-05-10 15:08:15 +0100

    chore: more on state, type unions

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// import GuestList from "./state/GuestList"
+import UserSearch from "./state/UserSearch";
+
+export const App = () => {
+  return (
+    // <GuestList/>
+    <UserSearch />
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from "react-dom";
-import GuestList from "./state/GuestList";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-const App = () => {
-  return (
-    <div>
-      <GuestList />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
